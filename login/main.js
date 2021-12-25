dc930e143cc5ea4e43ae89a25bc1829c53b334d1
bot_serives <84583691+neriwty@users.noreply.github.com>
2021-12-25 11:47:22 +0800

    011

---
+++ login/main.js
@@ -0,0 +1,9 @@
+function formSerialize(formElement) {
+    const values = {};
+    const inputs = formElement.elements;
+ 
+    for (let i = 0; i < inputs.length; i++) {
+        values[inputs[i].name] = inputs[i].value;
+    }
+    return values;
+ }
