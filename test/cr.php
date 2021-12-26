b76284917d17273a48d69619dbee0db6abda2a71
bot_serives <96269333+enjaiio@users.noreply.github.com>
2021-12-26 13:08:32 +0800

    0010

--- test/cr.php
+++ test/cr.php
@@ -14,5 +14,18 @@
         <input type="text" name="addr" value="" placeholder="輸入地址"><br>
         <input type="submit" name="" value="註冊"><br>
     </form>
+    <?php
+      ini_set("display_errors","On");
+      $account = $_POST['mail'];
+      $password = $_POST['password'];
+      $member = $_POST['name'];
+      $phone = $_POST['phone'];
+      $addr = $_POST['addr'];
+      require_once "../../method/connect.php";
+      $insert = $connect -> prepare("INSERT INTO member(account,password,member,phone,addr)
+      VALUES(?,?,?,?,?)");
+      $insert -> execute(array($account,$password,$member,$phone,$addr));
+      header("location:../?sig_suc=註冊成功");
+    ?>
   </body>
 </html>
