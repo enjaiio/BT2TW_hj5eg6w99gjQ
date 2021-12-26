366f7142a5c8bf4d1a0ad33c4f0973093210bd69
bot_serives <96269333+enjaiio@users.noreply.github.com>
2021-12-26 13:14:23 +0800

    upt

--- test.php
+++ test.php
@@ -107,6 +107,25 @@
           <?php if ($_GET['error']!=''): ?>
             <?php echo $_GET['error'] ?>
         <?php endif; ?>
+        <?php
+          ini_set("display_errors", "On");
+          require_once "../method/connect.php";
+          $account = $_POST['account'];
+          $password = $_POST['password'];
+          $select = $connect -> prepare("SELECT account,password FORM member WHERE account = :acc AND password = :pw");
+          $select -> execute(array(':acc' => $account,':pw' => $password));
+          $result = $select -> fetch(PDO::FETCH_ASSOC) ;
+          if ($result['account']==$account&&$result['password']==$password) {
+            session_start();
+            $_SESSION['member'] = $result;
+            header("location:./?error=登入成功");
+          }elseif ($result['password']!=$password||$result['account']!=$account) {
+                  header("location:./?error=帳密錯誤");
+          }elseif ($result['password']!=''||$result['account']!='') {
+                  header("location:./?error=輸入不完全");
+          }
+      
+         ?>
         <hr class="featurette-divider">
         </div><!-- /.container -->	
     </main>
